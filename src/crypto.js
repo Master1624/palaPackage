cd526ab6fbb09bf21ef529b3f06c8daaee19bbd0
Master1624 <jdpa83@gmail.com>
2025-06-06 10:24:26 -0500

    feat: Basics of crypto

---
+++ src/crypto.js
@@ -0,0 +1,6 @@
+const crypto = require("crypto");
+
+const text = "Hello, Crypto World!";
+const hash = crypto.createHash("sha256").update(text).digest("hex");
+console.log("Texto original:", text);
+console.log("Hash SHA-256:", hash);
