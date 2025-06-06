0e229fbb5a907819c5ee6881ca3877b49ea8e8e2
Master1624 <jdpa83@gmail.com>
2025-06-06 10:05:16 -0500

    feat: Count, group, asserts, clear and traces

--- src/console.js
+++ src/console.js
@@ -23,3 +23,33 @@ for (let i = 0; i < 1000000; i++) {
 }
 
 console.timeEnd("Operación");
+
+// Count
+console.count("Contador");
+console.count("Contador");
+console.count("Contador");
+console.count("Contador");
+console.count("Contador");
+console.countReset("Contador");
+console.count("Contador");
+
+// Agrupación
+console.group("Grupo principal");
+console.log("Información 1");
+console.group("Subgrupo de información 1");
+console.log("Información subgrupo 1");
+console.groupEnd();
+console.group("Subgrupo de información 2");
+console.log("Información subgrupo 2");
+console.groupEnd();
+console.log("Final");
+console.groupEnd();
+
+// Afirmaciones
+console.assert(1 === 1, "Esto no se mostrará");
+console.assert(1 === 2, "Esto se mostrará");
+
+// Limpiar
+console.clear();
+
+console.trace("Mostrar la pila de llamadas actual")
